609f97b03d8052dc1c901ff95ee1f3e3ab78ccaf
Nick-Eagles <nickeagles77@gmail.com>
2025-10-27 22:13:29 -0400

    Add a task using PythonOperator and use XComs in a sort of trivial way

--- first_airflow/.airflow/dags/very_basic.py
+++ first_airflow/.airflow/dags/very_basic.py
@@ -1,5 +1,6 @@
 from airflow import DAG
 from airflow.providers.standard.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
 from datetime import datetime, timedelta
 
 default_args = {
@@ -8,6 +9,13 @@ default_args = {
     "retry_delay": timedelta(minutes=2)
 }
 
+#   Trivial example of a function using XComs to push data, allowing exchange
+#   between tasks
+def get_time(ti):
+    ti.xcom_push(
+        key="current_time", value=datetime.now().strftime("%Y-%m-%d")
+    )
+
 with DAG(
         dag_id="very_basic",
         start_date=datetime(2025, 10, 12, 2),
@@ -15,8 +23,17 @@ with DAG(
         description="A one-task DAG to get comfortable with Airflow",
         default_args=default_args
     ) as dag:
-    task1 = BashOperator(
-        task_id="hello_world", bash_command="echo 'Hello World!'"
+
+    #   Get the current time
+    task1 = PythonOperator(
+        task_id="get_time",
+        python_callable=get_time
     )
 
-    task1
+    #   "Hello world" but use XComs to get the current time from task1
+    task2 = BashOperator(
+        task_id="hello_world",
+        bash_command="echo 'Hello World, it is currently {{ task_instance.xcom_pull(task_ids='get_time', key='current_time') }}!'"
+    )
+
+    task1 >> task2
